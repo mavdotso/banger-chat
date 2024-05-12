5820832fd7885ad3332e122309d8d785966860b8
mav <0xmav.eth@gmail.com>
2024-05-12 11:40:14 +0400

    init

---
+++ app/layout.tsx
@@ -0,0 +1,50 @@
+import type { Metadata, Viewport } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import ConvexClientProviders from '@/providers/ConvexClientProviders';
+import { ClerkLoaded, ClerkLoading } from '@clerk/nextjs';
+import LoadingLogo from '@/components/shared/LoadingLogo';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { ThemeProvider } from '@/components/ui/theme/theme-provider';
+import { Toaster } from '@/components/ui/sonner';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const viewport: Viewport = {
+    themeColor: 'DodgerBlue',
+};
+
+export const metadata: Metadata = {
+    title: 'banger.chat',
+    description: 'NFT-gated community chats for Fantasy.top',
+    generator: 'Next.js',
+    manifest: '/manifest.json',
+    authors: [{ name: '@mavdotso' }],
+    icons: [
+        { rel: 'apple-touch-icon', url: 'icon-192x192.png' },
+        { rel: 'icon', url: 'icon-192x192.png' },
+    ],
+};
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+                    <ConvexClientProviders>
+                        <ClerkLoading>
+                            <LoadingLogo />
+                        </ClerkLoading>
+                        <ClerkLoaded>
+                            <TooltipProvider>{children}</TooltipProvider>
+                            <Toaster richColors />
+                        </ClerkLoaded>
+                    </ConvexClientProviders>
+                </ThemeProvider>
+            </body>
+        </html>
+    );
+}
